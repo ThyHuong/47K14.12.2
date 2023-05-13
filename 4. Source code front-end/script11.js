3c62ce475d075367a3df6527d9eed25055fe0f2b
Đỗ Thị Thy Hương <94677663+ThyHuong@users.noreply.github.com>
2023-05-13 20:44:56 +0700

    Update script11.js

--- 4. Source code front-end/script11.js
+++ 4. Source code front-end/script11.js
@@ -5,3 +5,12 @@ function closepopup(id) {
 function cancel() {
 	document.getElementById("cancel-click").classList.remove("hide");
 }
+
+function passcheck() {
+	if (document.getElementById("old-pass").value=="123456789") {
+		document.getElementById("wrongpass").classList.remove("hide");
+	}
+	else {
+		window.location.href = "index11.html";
+	}
+}
